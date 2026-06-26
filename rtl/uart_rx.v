6955bd79d2fb544e94f565da6eabab34247d862d
wyatteyler <wyatteyler2904@gmail.com>
2026-06-25 20:22:45 -0700

    Rewrite uart_rx

--- rtl/uart_rx.v
+++ rtl/uart_rx.v
@@ -10,8 +10,10 @@ module uart_rx (
 	localparam START = 2'b01;
 	localparam DATA = 2'b10;
 	localparam STOP = 2'b11;
-	
-	wire start_detect;
+
+	wire sample_point;
+	wire bit_done;
+	wire start_detect; 
 	reg rx_flop;
 	reg rx_sync;
 	reg rx_prev;
@@ -22,6 +24,8 @@ module uart_rx (
 	reg [7:0] shift_reg;
 	
 	assign start_detect = ~rx_sync && rx_prev;
+ 	assign bit_done = (oversample_count == 4'd15) && baud_tick16;
+  	assign sample_point = (oversample_count == 4'd8) && baud_tick16;
 	
 	always @(posedge clk) begin
 		if (reset) begin
@@ -41,74 +45,51 @@ module uart_rx (
 			oversample_count <= 4'd0;
 			shift_reg <= 8'd0;
 			rx_prev <= 1'b1;
-		end
-		else begin
+        end
+      	else begin
 			rx_prev <= rx_sync;
 			current_state <= next_state;
-		end
-		if (start_detect && (current_state == IDLE)) begin		// possible redundancy 
-			bit_count <= 3'd0;
-			oversample_count <= 4'd0;
-			shift_reg <= 8'd0;
-			rx_prev <= 1'b1;
-		end
-		case (current_state)
-			START: begin
-				if (baud_tick16) begin
-					if (oversample_count == 4'd15) begin
-						oversample_count <= 4'b0;
-					end
-					else begin
-					oversample_count <= oversample_count + 1;
-					end
-				end
+			if (start_detect && current_state == IDLE) begin
+				oversample_count <= 4'd0;
+				bit_count <= 3'd0;
+				shift_reg <= 8'd0;
 			end
-			DATA: begin
-				if ((oversample_count == 4'd8) && baud_tick16) begin
+			if (baud_tick16 && current_state != IDLE)
+				oversample_count <= oversample_count + 1;
+          case (current_state)
+            DATA: begin
+				if (sample_point)
 					shift_reg <= {shift_reg[6:0], rx_sync};
-				end
-				if (baud_tick16) begin
-					if (oversample_count == 4'd15) begin
-						bit_count <= bit_count + 1;
-						oversample_count <= 4'b0;
-					end
-					else begin
-					oversample_count <= oversample_count + 1;
-					end
-				end
-			end
-			STOP: begin
-				if (baud_tick16) begin			// wrong
-					oversample_count <= 4'd0;
-				else
-					oversample_count <= oversample_count + 1;
-				end
-				if ((oversample_count == 4'd8) && rx_sync)
+				if (bit_done)
+					bit_count <= bit_count + 1;
+            end
+            STOP: begin
+				if (sample_point && rx_sync)
 					rx_dataout <= shift_reg;
-			end
-			default: ;
-		endcase
-	end
-	
+            end
+            default: ;
+          endcase
+        end
+    end
+
 	always @(*) begin
 		case (current_state)
 			IDLE: next_state = (start_detect) ? START : IDLE;
-			START: next_state = ((oversample_count == 4'd8) && (rx_sync == 1'b0)) ? DATA : START;
-			DATA: next_state = ((oversample_count == 4'd8) && (bit_count == 3'd7)) ? STOP : DATA;
-			STOP: next_state = ((oversample_count == 4'd8) && rx_sync) ? IDLE : STOP;
+         	START: next_state = (sample_point && (rx_sync == 1'b0)) ? DATA : START;
+         	DATA: next_state = (sample_point && (bit_count == 3'd7)) ? STOP : DATA;
+         	STOP: next_state = (sample_point && rx_sync) ? IDLE : STOP;
 			default: next_state = IDLE;
 		endcase
 	end
-	
+  
 	always @(*) begin
 		rx_datavalid = 1'b0;
 		case (current_state)
-			STOP: begin
-				if (baud_tick16) begin
-					rx_datavalid = 1'b1;
-				end
-			end
-			default: rx_datavalid = 1'b0;
+		STOP: 
+			if (sample_point && rx_sync)
+			rx_datavalid = 1'b1;
+		default:
+			rx_datavalid = 1'b0;
 		endcase
 	end
 endmodule
